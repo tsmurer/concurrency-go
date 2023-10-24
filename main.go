516c68a1c7c763e4fc37278fd192f6c6b521d3ea
tsmurer <tsmurer@Ubuntu-22.04.2.myguest.virtualbox.org>
2023-10-24 16:04:12 -0300

    concurrency with goroutines, channels and waitgroups

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	respch := make(chan string, 1024)
+	now := time.Now()
+
+	wg := &sync.WaitGroup{}
+
+	go fetchUserData(respch, wg)
+	wg.Add(1)
+	go fetchUserRecommendations(respch, wg)
+	wg.Add(1)
+	go fetchUserLikes(respch, wg)
+	wg.Add(1)
+
+	wg.Wait()
+
+	close(respch)
+
+	for resp := range respch {
+		fmt.Println(resp)
+	}
+	fmt.Println(time.Since(now))
+}
+
+func fetchUserData(respch chan string, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 80)
+	respch <- "userData"
+	wg.Done()
+}
+
+func fetchUserRecommendations(respch chan string, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 120)
+	respch <- "userRecommendations"
+	wg.Done()
+}
+
+func fetchUserLikes(respch chan string, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 50)
+	respch <- "userLikes"
+	wg.Done()
+}
